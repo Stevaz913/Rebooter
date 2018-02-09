d9963f565f563bd1ab992e0e0bdb6db1fd1a15f4
Steve Vega <Stevefreake@gmail.com>
2018-02-09 04:34:59 -0700

    changed stuff

--- rebooter.py
+++ rebooter.py
@@ -9,9 +9,9 @@ parser = SafeConfigParser()
 parser.read('rebooter.cfg')
 
 def delete_last_lines(n=1):
-    for _ in range(n):
-        sys.stdout.write(CURSOR_UP_ONE)
-        sys.stdout.write(ERASE_LINE)
+	for _ in range(n):
+		sys.stdout.write(CURSOR_UP_ONE)
+		sys.stdout.write(ERASE_LINE)
 
 #print('Current values:')
 #print ('A1: ' + parser.get('A1', 'reboot'))
@@ -26,16 +26,16 @@ testtime = 30
 while testime > 0:
 	parser.set('A1', 'reboot', 'False')
 	with open('rebooter.cfg', 'wb') as configfile:
-    	parser.write(configfile)
-    print('waiting' + testtime + ' seconds')
-    time.sleep(1)
+		parser.write(configfile)
+	print('waiting' + testtime + ' seconds')
+	time.sleep(1)
 
 testime = 10
 print('Sending Reboot')
 
 parser.set('A1', 'reboot', 'True')
 with open('rebooter.cfg', 'wb') as configfile:
-   	parser.write(configfile)
+	parser.write(configfile)
 
 while testime > 0:
 	print('Rebooting...' + testime + 'seconds remain')
@@ -43,4 +43,4 @@ while testime > 0:
 
 parser.set('A1', 'reboot', 'False')
 with open('rebooter.cfg', 'wb') as configfile:
-   	parser.write(configfile)
+	parser.write(configfile)
