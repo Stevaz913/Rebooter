edfceabfa90d212288db7ee0410648e63c6cd22c
Steve Vega <Stevefreake@gmail.com>
2018-02-09 04:32:32 -0700

    changed stuff

--- rebooter.py
+++ rebooter.py
@@ -13,19 +13,34 @@ def delete_last_lines(n=1):
         sys.stdout.write(CURSOR_UP_ONE)
         sys.stdout.write(ERASE_LINE)
 
-print('Current values:')
-print ('A1: ' + parser.get('A1', 'reboot'))
-print ('A2: ' + parser.get('A2', 'reboot'))
-print ('A3: ' + parser.get('A3', 'reboot'))
-print ('B1: ' + parser.get('B1', 'reboot'))
-print ('B2: ' + parser.get('B2', 'reboot'))
-print ('')
+#print('Current values:')
+#print ('A1: ' + parser.get('A1', 'reboot'))
+#print ('A2: ' + parser.get('A2', 'reboot'))
+#print ('A3: ' + parser.get('A3', 'reboot'))
+#print ('B1: ' + parser.get('B1', 'reboot'))
+#print ('B2: ' + parser.get('B2', 'reboot'))
+#print ('')
+
+testtime = 30
+
+while testime > 0:
+	parser.set('A1', 'reboot', 'False')
+	with open('rebooter.cfg', 'wb') as configfile:
+    	parser.write(configfile)
+    print('waiting' + testtime + ' seconds')
+    time.sleep(1)
+
+testime = 10
+print('Sending Reboot')
+
+parser.set('A1', 'reboot', 'True')
+with open('rebooter.cfg', 'wb') as configfile:
+   	parser.write(configfile)
+
+while testime > 0:
+	print('Rebooting...' + testime + 'seconds remain')
+	time.sleep(1)
 
 parser.set('A1', 'reboot', 'False')
-parser.set('A2', 'reboot', 'False')
-parser.set('A3', 'reboot', 'False')
-parser.set('B1', 'reboot', 'False')
-parser.set('B2', 'reboot', 'False')
-
 with open('rebooter.cfg', 'wb') as configfile:
-    parser.write(configfile)
+   	parser.write(configfile)
