4959e44eda0ba5470b214f3e3167b4efc3b2cce0
Steve Vega <Stevefreake@gmail.com>
2018-02-09 13:59:50 -0700

    Changed stuff

--- rb.py
+++ rb.py
@@ -1,10 +1,22 @@
 import rebooter
+import rball
 
 print("----------------------------------")
 print("---- REBOOT BOX v0.0.00000001 ----")
 print("----------------------------------")
 print("")
 print("Commands are A1, A2, etc.")
-rig = raw_input("====>  Send a reboot to: ")
+print("'All' to reboot all, Q to quit")
 
-rebooter.rebme(rig)
+while True:
+	rig = raw_input("====>  Reboot command: ")
+	if rig == 'A1' or 'A2' or 'A3' or 'B1' or 'B2':
+		rebooter.rebme(rig)
+	elif rig == 'Q' or 'q':
+		print('Exiting...')
+		sys.exit(0)
+	elif rig == 'All':
+		rball.rbooter()
+	else:
+		print(str(rig) + 'not a valid command')
+		print('')
--- rball.py
+++ rball.py
@@ -1,14 +1,6 @@
 import rebooter
 import time
 
-i = 60
-print("-----------------------------------")
-print("----- REBOOT BOX v0.0.00000001 ----")
-print("-----------------------------------")
-print("")
-print("-----  REBOOTING ALL RIGS  --------")
-
-
 def rbstation(rig):
 	i = 60
 	rebooter.rebme(rig)
@@ -18,10 +10,11 @@ def rbstation(rig):
 		rebooter.delete_last_lines(1)
 		time.sleep(1)
 
-rbstation('A1')
-rbstation('A2')
-rbstation('A3')
-rbstation('B1')
-rbstation('B2')
+def rbooter():
+	rbstation('A1')
+	rbstation('A2')
+	rbstation('A3')
+	rbstation('B1')
+	rbstation('B2')
 
 print('All Rebooted Probly')
