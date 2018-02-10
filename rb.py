5915348a1e162aa6318b2e44b5ede65c5939c2d4
Steve Vega <Stevefreake@gmail.com>
2018-02-10 04:16:18 -0700

    Fixed status naming mechanism, cleaned up code

--- rb.py
+++ rb.py
@@ -5,41 +5,45 @@ import sys
 import time
 import urllib, urllib2, json, requests
 import textwrap
+import string
 
-ret = urllib2.urlopen(urllib2.Request('http://vega07.ethosdistro.com/?json=yes'))
-data = json.loads(ret.read())
+def minerStatus(rig):
+	status = data['rigs'][rig]['condition']
+	if status == 'mining':
+		x = status.replace('mining', 'OK      ')
+	elif status == 'stuck_miners':
+		x = status.replace('stuck_miners', 'STUCK   ')
+	elif status =='just_booted':
+		x = status.replace('just_booted', 'BOOTUP  ')
+	else:
+		x = status
+	return x
 
-a1stat = data['rigs']['5026ef']['condition']
-a2stat = data['rigs']['50270d']['condition']
-a3stat = data['rigs']['482892']['condition']
-b1stat = data['rigs']['502b8a']['condition']
-b2stat = data['rigs']['590b29']['condition']
-
-a1gpu = data['rigs']['5026ef']['gpus']
-a2gpu = data['rigs']['50270d']['gpus']
-a3gpu = data['rigs']['482892']['gpus']
-b1gpu = data['rigs']['502b8a']['gpus']
-b2gpu = data['rigs']['590b29']['gpus']
+def minerGPUs(rig):
+	status = data['rigs'][rig]['miner_instance']
+	return status
 
 while True:
+	ret = urllib2.urlopen(urllib2.Request('http://vega07.ethosdistro.com/?json=yes'))
+	data = json.loads(ret.read())
 	os.system('clear')
 	print("---------------------------------------------")
-	print("|------------ REBOOT BOX v0.0.11 -----------|")
+	print("|------------ RIG PANEL v0.1.03  -----------|")
 	print("|-------------------------------------------|")
 	print("|                                           |")
-	print("|    A1 Status: " + str(a1stat) + " | " + str(a1gpu) + "/7 GPUs Running   |")
-	print("|    A2 Status: " + str(a2stat) + " | " + str(a2gpu) + "/7 GPUs Running   |")
-	print("|    A3 Status: " + str(a3stat) + " | " + str(a3gpu) + "/9 GPUs Running   |")
-	print("|    B1 Status: " + str(b1stat) + " | " + str(b1gpu) + "/9 GPUs Running   |")
-	print("|    B2 Status: " + str(b2stat) + " | " + str(b2gpu) + "/8 GPUs Running   |")
+	print("|  A1 Status: " + minerStatus('5026ef') + "|  " + minerGPUs('5026ef') + "/7 GPUs Running   |")
+	print("|  A2 Status: " + minerStatus('50270d') + "|  " + minerGPUs('50270d') + "/7 GPUs Running   |")
+	print("|  A3 Status: " + minerStatus('482892') + "|  " + minerGPUs('482892') + "/9 GPUs Running   |")
+	print("|  B1 Status: " + minerStatus('502b8a') + "|  " + minerGPUs('502b8a') + "/9 GPUs Running   |")
+	print("|  B2 Status: " + minerStatus('590b29') + "|  " + minerGPUs('590b29') + "/8 GPUs Running   |")
 	print("|                                           |")
-	print("|          Commands are A1, A2, etc.        |")
-	print("|       'stats' to update miner status      |")
-	print("|       'All' to reboot all, Q to quit      |")
+	print("|      Reboot commands are A1, A2, etc.     |")
+	print("|      'stats' to update miner status       |")
+	print("|      'All' to reboot all, Q to quit       |")
 	print("---------------------------------------------")
 	print("")
 	
-	rig = raw_input("====>  Reboot command: ")
+	rig = raw_input("====>  Panel Command: ")
 	
 	if rig == 'A1' or rig == 'A2' or rig == 'A3' or rig == 'B1' or rig == 'B2':
 		rebooter.rebme(rig)
@@ -69,11 +73,11 @@ while True:
 		a3stat = data['rigs']['482892']['condition']
 		b1stat = data['rigs']['502b8a']['condition']
 		b2stat = data['rigs']['590b29']['condition']
-		a1gpu = data['rigs']['5026ef']['gpus']
-		a2gpu = data['rigs']['50270d']['gpus']
-		a3gpu = data['rigs']['482892']['gpus']
-		b1gpu = data['rigs']['502b8a']['gpus']
-		b2gpu = data['rigs']['590b29']['gpus']
+		a1gpu = data['rigs']['5026ef']['miner_instance']
+		a2gpu = data['rigs']['50270d']['miner_instance']
+		a3gpu = data['rigs']['482892']['miner_instance']
+		b1gpu = data['rigs']['502b8a']['miner_instance']
+		b2gpu = data['rigs']['590b29']['miner_instance']
 		print('Updating miner stats....')
 		time.sleep(1)
 		print('Done')
