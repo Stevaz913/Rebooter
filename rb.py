d97161641a6daf2403972e5c672e3a7923c59279
Steve Vega <Stevefreake@gmail.com>
2018-02-09 17:24:43 -0700

    Changed stuff

--- rb.py
+++ rb.py
@@ -3,6 +3,7 @@ import rball
 import os
 import sys
 import time
+import urllib, json, requests
 
 while True:
 	os.system('clear')
@@ -14,14 +15,27 @@ while True:
 	print("| 'All' to reboot all, Q to quit |")
 	print("----------------------------------")
 	print("")
+	
 	rig = raw_input("====>  Reboot command: ")
+	
 	if rig == 'A1' or rig == 'A2' or rig == 'A3' or rig == 'B1' or rig == 'B2':
 		rebooter.rebme(rig)
+	
 	elif rig == 'Q' or rig == 'q':
 		print('Exiting...')
 		sys.exit(0)
+	
 	elif rig == 'All':
 		rball.rbooter()
+	
+	elif rig == 'dadjoke':
+		response = requests.get("https://icanhazdadjoke.com/",
+  		  headers={
+    	    "Accept": "application/json"
+  		  }
+		)
+		print response.json()['joke']
+
 	else:
 		print("")
 		print("     " + str(rig) + ' is not a valid command')
