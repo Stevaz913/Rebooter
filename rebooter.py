e50abe9ca2fd83d8ce046163ebb6685e87772b99
Steve Vega <Stevefreake@gmail.com>
2018-02-11 10:00:11 -0700

    Cleaned code redundancies

--- rebooter.py
+++ rebooter.py
@@ -16,14 +16,6 @@ def delete_last_lines(n=1):
 		sys.stdout.write(CURSOR_UP_ONE)
 		sys.stdout.write(ERASE_LINE)
 
-#print('Current values:')
-#print ('A1: ' + parser.get('A1', 'reboot'))
-#print ('A2: ' + parser.get('A2', 'reboot'))
-#print ('A3: ' + parser.get('A3', 'reboot'))
-#print ('B1: ' + parser.get('B1', 'reboot'))
-#print ('B2: ' + parser.get('B2', 'reboot'))
-#print ('')
-
 def rebme(rig):
 	testime = 10
 	print('Sending Reboot to ' + rig)
